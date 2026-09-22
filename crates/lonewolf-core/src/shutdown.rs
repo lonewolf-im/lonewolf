9de500056a3604cad0896d33d5ee10319dcc9229
Miguel Ángel Ortuño <888899+ortuman@users.noreply.github.com>
2026-09-22 12:32:33 +0200

    feat: log stop signal lifecycle (#39)
    Signed-off-by: Miguel Ángel Ortuño <ortuman@gmail.com>

--- crates/lonewolf-core/src/shutdown.rs
+++ crates/lonewolf-core/src/shutdown.rs
@@ -9,7 +9,12 @@ use nix::sys::signal::Signal;
 pub(crate) async fn wait() -> io::Result<()> {
     let interrupt = pin!(compio::signal::ctrl_c());
     let terminate = pin!(compio::signal::unix::signal(Signal::SIGTERM as i32));
-    match select(interrupt, terminate).await {
+    tracing::info!("waiting for stop signal... (press Ctrl+C to stop the server)");
+    let result = match select(interrupt, terminate).await {
         Either::Left((result, _)) | Either::Right((result, _)) => result,
+    };
+    if result.is_ok() {
+        tracing::info!("received stop signal... gracefully shutting down...");
     }
+    result
 }
